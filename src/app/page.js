60aeff751fe766344da12c3911cc1ad581f1bc55
davide112233 <davidetagini4@gmail.com>
2025-04-10 18:47:21 +0200

    added fadeout animation with opacity after loading

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,21 @@
 import HangmanGame from "./components/HangmanGame";
 import MainMenu from "./components/MainMenu";
 import useGameStore from "./utils/useGameStore";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const difficulty = useGameStore((state) => state.difficulty);
   
   return (
     <main>
-      {difficulty ? <HangmanGame /> : <MainMenu />}
+      <motion.div
+        key={difficulty ? "game" : "menu"}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 4.3, type: "spring" }}
+        animate={{ opacity: 1 }}
+      >
+        {difficulty ? <HangmanGame /> : <MainMenu />}
+      </motion.div>
     </main>
   );
 }
